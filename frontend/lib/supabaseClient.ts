51b69546062ac44365cd3ca51477a2105c955578
Giridhar Chettiar <giri.chettiar@gmail.com>
2025-03-18 08:54:41 +1030

    SuperbaseClient update with correct .env variables

--- frontend/lib/supabaseClient.ts
+++ frontend/lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js"
 
 // Replace with your own project URL & anon key
-const supabaseUrl = process.env.supabaseUrl || ''
-const supabaseAnonKey = process.env.supabaseAnonKey || ''
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || ''
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
